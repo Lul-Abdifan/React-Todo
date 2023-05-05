f66b74aaa70289f442d31247bc28c0d7b4dfa593
Lul-Abdifan <tasanbannu@gmail.com>
2023-05-05 06:25:09 -0400

    Update README file

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React from "react";
 
 function About() {
   return (
     <div className="about">
       <h1>We’re building the future of work</h1>
       <p>
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum.
+        To-do list Web which is done for educational purpose and can help you
+        get, and stay, on top of important projects and piles of tasks or
+        decisions.
       </p>
     </div>
   );
